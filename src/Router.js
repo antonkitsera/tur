f48e3551ea75c8349fad7eddb6540a0dea405f59
Anton Kitsera <antonkitsera@gmail.com>
2020-02-24 12:31:25 +0200

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -22,23 +22,6 @@ import RequestsPage from "./AdminPages/Requests";
 const Router = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={Routers}/>
-            <Route path='/category/:id' component={Routers}/>
-            <Route path='/item/:id' component={Routers}/>
-            <Route path='/ordering' component={Routers}/>
-            <Route path='/delivery_and_pay' component={Routers}/>
-            <Route path='/about_your_personal_account' component={Routers}/>
-            <Route path='/publishing' component={Routers}/>
-            <Route path='/author' component={Routers}/>
-            <Route path='/author/:id' component={Routers}/>
-            <Route path='/publishing/:id' component={Routers}/>
-            <Route path='/all_items' component={Routers}/>
-            <Route path='/item/search_result/:id' component={Routers}/>
-            <Route path='/category/:id/all_in_category' component={Routers}/>
-            <Route path='/about_the_books' component={Routers}/>
-            <Route path='/category/:id/pre_category/:id' component={Routers}/>
-            <Route exact path='/admin' component={Login}/>
-            <PrivateRoute path='/personal_account'/>
             <Route path='/404' component={Routers}/>
 
             <Route exact path='/admin' component={Login}/>
@@ -55,7 +38,7 @@ const Router = () => (
             <ProtectedAdminRoute exact path='/admin/users/:id?' component={UsersPage}/>
             <ProtectedAdminRoute exact path='/admin/requests' component={RequestsPage}/>
 
-            <Redirect to='/404'/>
+            <Redirect to='/admin'/>
         </Switch>
     </BrowserRouter>
 );
